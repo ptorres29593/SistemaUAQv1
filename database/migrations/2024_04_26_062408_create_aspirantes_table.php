167746963f8ac3070196348b3e9098fc74f9645b
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-04-26 00:35:16 -0600

    add: Table Aspirantes

---
+++ database/migrations/2024_04_26_062408_create_aspirantes_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aspirantes', function (Blueprint $table) {
+            $table->id();
+            $table->mediumInteger('clave');
+            $table->string('nombre');
+            $table->string('apellidoPaterno');
+            $table->string('apellidoMaterno');
+            $table->date('fechaNacimiento');
+            $table->smallInteger('nacionalidad');
+            $table->string('posgrado');
+            $table->string('estadoCivil');
+            $table->tinyInteger('estadoNacimiento');
+            $table->string('lugarNacimientoExtranjero');
+            $table->string('telefonoFijo');
+            $table->string('telefonoMovil');
+            $table->string('email');
+            $table->string('direccionCalle');
+            $table->string('direccionNumero');
+            $table->string('direccionColonia');
+            $table->string('direccionCP');
+            $table->integer('direccionMunicipio');
+            $table->tinyInteger('direccionEstado');
+            $table->string('universidad');
+            $table->string('universidadDireccion');
+            $table->string('licenciatura');
+            $table->string('fechaIngreso');
+            $table->string('fechaEgreso');
+            $table->float('promedioGeneral');
+            $table->tinyInteger('tieneTitulo');
+            $table->string('LGAC');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aspirantes');
+    }
+};
