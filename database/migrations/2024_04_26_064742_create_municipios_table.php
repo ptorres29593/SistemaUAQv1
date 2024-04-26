35e4b668b6861f76d3e07894f7b9f66f34a688c0
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-04-26 00:52:16 -0600

    add: Table pais, estado, municipio

---
+++ database/migrations/2024_04_26_064742_create_municipios_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('municipios', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idEstado'); // Foreign Key
+            $table->string('clave');
+            $table->string('municipio');
+            $table->string('sigla');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('municipios');
+    }
+};
