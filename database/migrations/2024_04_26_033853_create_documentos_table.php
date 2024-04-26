81175e839fee466ded074714bab5cb2e9e191c56
amnzmj <amunoz31965@ucq.edu.mx>
2024-04-25 21:56:26 -0600

    tabla documento

---
+++ database/migrations/2024_04_26_033853_create_documentos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documentos', function (Blueprint $table) {
+            $table->bigIncrements('idDocumento'); //bigint(20)
+            $table->bigInteger('idExpediente');//int(11)
+            $table->tinyInteger('idTipoDocumento');//tinyint(4)
+            $table->tinyInteger('idExtension')->nullable();//tinyint(4)
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documentos');
+    }
+};
