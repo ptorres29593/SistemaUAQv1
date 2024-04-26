38f9003d0ec2ebae0ac83ae0b8000bc0c80ceb2b
amnzmj <amunoz31965@ucq.edu.mx>
2024-04-25 21:26:20 -0600

    tabla administrador

---
+++ database/migrations/2024_04_26_025232_create_administradors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('administradors', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('nombre');
+            $table->string('password');
+            $table->string('email');
+            $table->string('posgradoAdm')->nullable();
+            $table->tinyInteger('notificar');//boolean tal vez
+            $table->tinyInteger('activo');//boolean tal vez
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('administradors');
+    }
+};
